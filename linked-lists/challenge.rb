081517fc7955c88345a97695d35679c9986a6be7
Jamezjaz <jamezjaz@gmail.com>
2021-06-30 00:15:34 +0100

    docs: Adds linked lists algorithm

---
+++ linked-lists/challenge.rb
@@ -0,0 +1,14 @@
+# Start with your code from last challenge.
+
+list = LinkedList.new
+
+list.add(3)
+list.add(5)
+list.add_at(1, 11)
+list.add_at(0, 13)
+
+puts list.get(2)
+# => 11
+
+puts list.get(3)
+# => 5
